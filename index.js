13d40702ad9513967c50f153973415753baea3ea
Martin Moy <martinmoy1991@gmail.com>
2019-10-16 20:30:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ main.remove();
 
 let newHeader = document.createElement('h1');
 newHeader.setAttribute('id', 'victory');
-newHeader.textContent = 'is the champion';
+newHeader.textContent = 'Martin is the champion';
