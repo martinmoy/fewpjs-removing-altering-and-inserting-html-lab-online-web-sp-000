b7e04cd32cc00ea6aac572e054da40a1b107abf9
Martin Moy <martinmoy1991@gmail.com>
2019-10-16 20:31:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = 'Martin is the champion';
